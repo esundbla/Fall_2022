f81b092a7564d8ea60deb846488f28530a67628a
esundbla <69823637+esundbla@users.noreply.github.com>
2022-08-25 12:00:43 -0600

    Week 1 course work

---
+++ Algorithms/C_examples/hello.c
@@ -0,0 +1,8 @@
+/*
+* File: hello.c
+*/
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Welcome back commander");
+}
---
+++ PPL/hw1/ScalaBasics.scala
@@ -0,0 +1,28 @@
+/*
+ * CS3210 - Principles of Programming Languages - Fall 2022
+ * Instructor: Thyago Mota
+ * Student: Erik Sundblad
+ * Description: Homework 01 - Scala Basics
+ */
+
+object ScalaBasics {
+
+  def main(args: Array[String]): Unit = {
+
+    // TODO #1: declare an integer constant named MAX and set it to 10; declare an integer variable named sum and initialized it to 0
+    var MAX = 10;
+    var sum = 0;
+    // TODO #2: write a for loop to compute sum as 1 + 2 + ... + MAX; then, display "sum is ..." substituting with the value of sum (must use variable substitution)
+    for( x <- sum to MAX){
+      sum = sum + x;
+    }
+    println("sum is " + sum)
+    // TODO #3: write a conditional to display "sum is even!" or "sum is odd!" depending on the value of sum;
+    if(sum % 2 == 0){
+      println("sum is even!");
+    }
+    else{
+      println("sum is odd!");
+    }
+  }
+}
