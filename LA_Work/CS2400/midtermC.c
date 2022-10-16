b755d78dbffdc61c79ffa214b0e53b9947d6e9f5
esundbla <69823637+esundbla@users.noreply.github.com>
2022-10-15 19:27:06 -0600

    Numerical

--- LA_Work/CS2400/midtermC.c
+++ LA_Work/CS2400/midtermC.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+#include <stdio.h>  //C library for input output
 
 
-int main(){
-    int x, y, z;
-    x=1;
-    y=0;
-    z=7;
+int main(){         // Main text of program
+    int x, y, z;    
+    x=1;            //$s1 = 1
+    y=0;            //$s2 = 0
+    z=7;            //$s3 = 7
 
-    if( z*y < x){
-        print("Less than");
+    if( z*y < x){               
+        printf("Less than");     //Syscall
     }else
-        print("greater than");
-    return 0;
+        printf("greater than");  //Syscall 
+    return 0;                    //Exit Syscall
 }
