3f6afd4d1f7dedc23f4dd43d5117501ee6f82c2f
esundbla <69823637+esundbla@users.noreply.github.com>
2022-10-17 20:21:42 -0600

    midterm readiness

--- LA_Work/CS2400/midtermC.c
+++ LA_Work/CS2400/midtermC.c
@@ -15,7 +15,8 @@ int main(){
 }
 */
 void main(){
-    int x, e;                                       //Two variables to hold input $s0, $s1
+    int x, e;
+    int r = 1;                                       //Three variables to hold input $s0, $s1, $s2
     
     printf("Input the value of the base: ");        //Ask for user input
     scanf("%d", &x);                                //Store input into x
@@ -23,11 +24,11 @@ void main(){
     printf("Input the value of the exponent: ");    //Ask for user input
     scanf("%d", &e);                                //Store input into e
 
-    int i  = 1;                                     //Counter variable initialized to 1 
+    int i  = 0;                                     //Counter variable initialized to 1 $s3
     for(i;i<e;i++){                                 //loop wile 1 < e
-        e = e*e;                                    //opperation
+        r = e*r;                                    //opperation
     }
 
-    printf("The final result is: %d", e);            //Output the results
+    printf("The final result is: %d", r);            //Output the results
 
 }
