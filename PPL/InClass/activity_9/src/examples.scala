b9e0eef90ab285a44bbc8f18169ae5ff90fbcb78
esundbla <69823637+esundbla@users.noreply.github.com>
2022-10-25 10:31:58 -0600

    bat

--- PPL/InClass/activity_9/src/examples.scala
+++ PPL/InClass/activity_9/src/examples.scala
@@ -9,6 +9,23 @@ object Examples {
   
 
   def main(args: Array[String]): Unit = {
+    def add3(a: Int): Int => Int => Int = (b:Int) => (c:Int) => a + b+ c
+    def add4(d: Int): Int => Int => Int => Int = (a: Int) => (b: Int) =>  (c: Int) => d + add3(a)(b)(c)
+
+    //println(add3(4)(5)(6))
+    //println(add4(4)(5)(6)(7))
+
+    def gcd(a: Int): Int => Int = (b: Int) => if (a%b == 0) b else gcd(b)(a%b)
+
+    def coprime(a: Int)= (b: Int) => gcd(a)(b) == 1
+
+    def totientPhi(m: Int) =
+      (1 until m).filter((r: Int) => coprime(r)(m))
+    print()
+
+    println(totientPhi(15))
+    println(coprime(2)(4))
+    println(gcd(1)(3))
 
   }
 }
